3afd340bf56a49ff5243ed862bd501aca889c2ca
Sahaj Patel <sahajyp@gmail.com>
2022-03-16 22:59:33 -0400

    Runner and interface for perfect square functionality created

--- Runner.java
+++ Runner.java
@@ -1,5 +1,13 @@
+import java.lang.Math;
+
+interface BooleanPart
+{
+	public boolean BooleanPart(int n);
+}
+
 public class Runner{
 	public static void main(String[]args){
-
+		BooleanPart booleanLambdas = (int n) -> { return (Math.sqrt(n) % 1 == 0); };
+		System.out.println(booleanLambdas.BooleanPart(26));
 	}
 }
